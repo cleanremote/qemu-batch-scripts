e3654dba608bf881d28a39acfaa178289a8718f9
cleanremote <cleanremote@mailinator.com>
2021-02-25 19:14:16 +0100

    Update Create Qemu Image.bat

--- Create Qemu Image.bat
+++ Create Qemu Image.bat
@@ -1,4 +1,4 @@
 @setlocal
-@set/PNAME=What do you want to name the disk 
-@set/PSIZE=How big would you like the disk to be? (In Gb) 
+@set/PNAME=What do you want to name the disk? 
+@set/PSIZE=How big would you like the disk to be? (in GB) 
 @qemu\qemu-img create -f raw "%USERPROFILE%\Desktop\%NAME%.img" %SIZE%G
